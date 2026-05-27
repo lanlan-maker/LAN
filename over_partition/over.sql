f24819230d105f973fa2d7c31dd10832d86a7c17
Антонина <lanlan@inbox.ru>
2026-05-27 11:22:24 +0300

    Update over.sql

--- over_partition/over.sql
+++ over_partition/over.sql
@@ -1,16 +1,23 @@
 # Вычислить разницу в "день,ч:м:с" между временем выполнения заданий
+    , где разница между  выполнением не более суток. 
+ # Легко вычислить любого студента, поменяв только Имя
 
-select student_name as Студент
-, concat(left(step_name,20),'...') as Шаг, result as Результат
-, from_unixtime(submission_time) as Дата_отправки
-,  sec_to_time(
-    
-  submission_time - lag(submission_time,1,submission_time)
-    over (order by submission_time)  )    
-     as Разница
-from step_student
+select Студент, Шаг, Дата_отправки, Разница
+
+from  (select student_name as Студент, concat(left(step_name,20),'...') as Шаг
+, result as Результат, from_UNIXTIME(submission_time) as Дата_отправки
+, (submission_time - lag(submission_time, 1 , submission_time) 
+    over (partition by student_id order by submission_time)) as Секунды_разницы
+
+, sec_to_time(submission_time - lag(submission_time, 1 , submission_time) 
+    over (partition by student_id order by submission_time)) as Разница         
+from step_student st
 join student using(student_id)
-join step using(step_id)
-where student_name = 'student_61'
-order by submission_time
+join step using(step_id) 
+       ) as tmp
+
+where Секунды_разницы <=86400
+and Студент = 'student_43'
+;
+
 ;
