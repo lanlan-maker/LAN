e23b0c6ba320860a69ad412745a0cb5695bd6b37
Антонина <lanlan@inbox.ru>
2026-05-27 11:25:42 +0300

    Update over.sql

--- over_partition/over.sql
+++ over_partition/over.sql
@@ -3,21 +3,20 @@
  -- Легко вычислить любого студента, поменяв только Имя
 
 select Студент, Шаг, Дата_отправки, Разница
-
-from  (select student_name as Студент, concat(left(step_name,20),'...') as Шаг
+from  
+   (select student_name as Студент, concat(left(step_name,20),'...') as Шаг
 , result as Результат, from_UNIXTIME(submission_time) as Дата_отправки
 , (submission_time - lag(submission_time, 1 , submission_time) 
     over (partition by student_id order by submission_time)) as Секунды_разницы
 
 , sec_to_time(submission_time - lag(submission_time, 1 , submission_time) 
-    over (partition by student_id order by submission_time)) as Разница         
-from step_student st
+    over (partition by student_id order by submission_time)) as Разница  
+   
+from step_student
 join student using(student_id)
 join step using(step_id) 
-       ) as tmp
+    ) as tmp
 
 where Секунды_разницы <=86400
 and Студент = 'student_43'
-;
-
 ;
