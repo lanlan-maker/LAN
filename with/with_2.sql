6ee8e2e1913295e5e7f16dd3b582ba96cf6343a3
Антонина <lanlan@inbox.ru>
2026-05-31 15:23:58 +0300

    Update with_2.sql

--- with/with_2.sql
+++ with/with_2.sql
@@ -2,35 +2,44 @@
 -- пользователь отправлял последнее верно выполненное задание каждого урока.
 -- Учитывать только студентов, прошедших хотя бы один шаг из всех трех уроков
 
-with count_step as
-(    select student_id
-        , lesson_id,    step_id,    submission_time                    -- для связки --
-    , concat(module_id,'.',lesson_position)           as Урок
-    , max(submission_time)
-         over(partition by lesson_id, student_id)     as max_sub
-    from step_student
-    join step using(step_id)
-    join lesson using(lesson_id)      where result = 'correct'
-),
-    find_step as
-(    select student_id  
-         , lesson_id,    step_id,    max_sub                           -- для связки --
-     , count(lesson_id) over (partition by student_id) as lesn_count  -- кол_во уроков --
-     , Урок
-     , from_unixtime(submission_time)                   as Макс_время_отправки 
-     , lag(max_sub)
-         over (partition by student_id order by max_sub) as before_max
-     from count_step
-     where submission_time = max_sub
- )
+with get_max as
+(  select student_id
+, lesson_id
+, max(submission_time) as max_sub
+from step_student
+join step using(step_id)
+where result ='correct'
+group by student_id, lesson_id    )
+    ,
+get_before_max_count_les as
+(  select student_id
+, lesson_id
+, max_sub
+, lag(max_sub) 
+ over (partition by student_id order by max_sub)
+ as before_max
+ , count(lesson_id) 
+over (partition by student_id) as count_les
+ from get_max   )
+,
+get_three_les as
+(  select student_id 
+, lesson_id
+, max_sub
+, before_max
+from get_before_max_count_les
+where count_les = 3  )
 
-select student_name as Студент 
-, Урок
-, Макс_время_отправки
-, ifnull(( ceil((max_sub-before_max)/86400) ),'-') as Интервал
-from find_step 
-join student using(student_id)
-join lesson using(lesson_id) 
-where  lesn_count >=3
-order by 1, 3           
-;
+select student_name
+, concat(module_id,'.',lesson_position) as Урок
+, from_unixtime(max_sub) as Пос_отпр
+,  ifnull(ceil(
+ (max_sub - before_max)/86400),'-') as Интервал
+ 
+from get_three_les
+ join student using(student_id)
+ join lesson using(lesson_id)
+
+order by 1, 3
+
+ ; 
